b6611d27999e9e1584e8f950793442940e318273
TripleA55 <102666466+TripleA55@users.noreply.github.com>
2022-08-31 22:59:19 -0500

    Initial commit

---
+++ app/src/main/java/com/pa/jueves/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pa.jueves;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
